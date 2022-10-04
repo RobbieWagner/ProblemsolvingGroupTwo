174148da37fc31479580619b9bbe886fd1719192
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-04 12:36:36 -0500

    solution found :D

--- src/MICS_2015_Problem_3.java
+++ src/MICS_2015_Problem_3.java
@@ -7,12 +7,15 @@ public class MICS_2015_Problem_3 {
         Scanner scan = new Scanner(System.in);
 
         String[] characters = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ".", "?", "\n"};
+        String output = "";
 
-        while(scan.hasNextLine())
+        while(scan.hasNextInt())
         {
-
+            int character = scan.nextInt();
+            output += characters[character % 30];
         }
 
+        System.out.println(output);
         scan.close();
     }
 }
