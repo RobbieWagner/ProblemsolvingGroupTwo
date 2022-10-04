7254ef9460c54fc114da1c37d8358779d173488a
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-04 12:17:18 -0500

    frame for input

---
+++ src/MICS_2015_Problem_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MICS_2015_Problem_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        while(scan.hasNextLine())
+        {
+
+        }
+
+        scan.close();
+    }
+}
