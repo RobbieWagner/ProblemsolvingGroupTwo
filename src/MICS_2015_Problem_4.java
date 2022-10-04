1d145a169f2fe3d9776b7cde200b1af03b781e5d
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-04 12:52:37 -0500

    frame

--- src/MICS_2015_Problem_4.java
+++ src/MICS_2015_Problem_4.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class MICS_2015_Problem_4 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        int divisors = 2;
+
+        scan.close();
     }
 }
