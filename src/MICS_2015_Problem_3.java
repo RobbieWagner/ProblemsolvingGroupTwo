535d3a5cabb73a2472df198bf573c2856e46ca21
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-04 12:23:24 -0500

    Da Alphabet

--- src/MICS_2015_Problem_3.java
+++ src/MICS_2015_Problem_3.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class MICS_2015_Problem_3 {
+    //Steganography problem, solve using modulus and an array of char
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
+        String[] characters = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ".", "?", "\n"};
+
         while(scan.hasNextLine())
         {
 
