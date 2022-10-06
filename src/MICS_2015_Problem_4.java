b18212e68b9f67bbf89a8caae4023df81c8aa8d4
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-05 21:03:12 -0500

    Solution w/o memoization Problem 4 2015

--- src/MICS_2015_Problem_4.java
+++ src/MICS_2015_Problem_4.java
@@ -6,8 +6,36 @@ public class MICS_2015_Problem_4 {
 
         int input = scan.nextInt();
 
-        int divisors = 2;
+        int output = countNumbersWith8Divisors(input);
+
+        System.out.println(output);
+
 
         scan.close();
+    }
+
+    public static int countNumbersWith8Divisors(int number){
+
+        if(number < 24)
+            return 0;
+
+        int numbersWith8Divisors = 0;
+        for(int i = 24; i <= number; i++)
+        {
+            if(numberHas8Divisors(i))
+                numbersWith8Divisors++;
+        }
+
+        return numbersWith8Divisors;
+    }
+
+    public static boolean numberHas8Divisors(int number){
+        int divisors = 2;
+
+        for(int i = 2; i <= number/2; i++){
+            if(number % i == 0) divisors++;
+        }
+        if(divisors == 8) return true;
+        return false;
     }
 }
