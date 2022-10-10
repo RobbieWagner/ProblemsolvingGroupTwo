6f6f09bd5a316fe02db96ff2fe4691f4a4bb8bda
Jay32 <32678728+Jayjayjay3000@users.noreply.github.com>
2022-10-09 22:26:10 -0500

    Optimized it a bit
    Reduced the length of the factorization loop by adding a factor after the loop if the remaining number variable is greater than 1.
    
    Also made some small changes to make the code a little more neat.

--- src/MICS_2015_Problem_4.java
+++ src/MICS_2015_Problem_4.java
@@ -14,25 +14,23 @@ public class MICS_2015_Problem_4 {
         scan.close();
     }
 
-    public static int countNumbersWith8Divisors(int number){
+    public static int countNumbersWith8Divisors(int number) {
 
         if(number < 24)
             return 0;
 
         int numbersWith8Divisors = 0;
-        for(int i = 24; i <= number; i++)
-        {
+        for(int i = 24; i <= number; i++) {
             ArrayList<Integer> powers = new ArrayList<>();
             int divisors = PrimeFactorize(i, powers);
-            if (divisors >= 8)
-            {
+            if (divisors == 8) {
                 numbersWith8Divisors++;
             }
         }
 
         return numbersWith8Divisors;
     }
-//
+
 //    public static int countDivisors(int number){
 //        int divisors = 2;
 //
@@ -44,32 +42,31 @@ public class MICS_2015_Problem_4 {
 
     public static int PrimeFactorize(int input, List<Integer> powers) {
         int number = input;
-        int powersIndex = 0;
         powers.add(0);
 
-        while(number % 2 == 0){
+        while(number % 2 == 0) {
             number /= 2;
             powers.set(0, powers.get(0) + 1);
         }
-        for(int i = 3; i < input/2; i+=2){
-
+        for(int currentFactor = 3; currentFactor <= Math.sqrt(number); currentFactor += 2) {
             int currentIndex = powers.size();
             powers.add(0);
 
-            while(number % i == 0){
-                number /= i;
+            while(number % currentFactor == 0) {
+                number /= currentFactor;
                 powers.set(currentIndex, powers.get(currentIndex) + 1);
             }
         }
 
+        // The remaining number is either 1 or the final factor
+        if(number != 1) {
+            powers.add(1);
+        }
+
         int result = 1;
 
-        for(int power : powers)
-        {
-            if(power > 0)
-            {
-                result *= power + 1;
-            }
+        for(int power : powers) {
+            result *= power + 1;
         }
 
         return result;
