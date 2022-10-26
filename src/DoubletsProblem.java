4d0ec3060a841cead22f0b2d6139c47939ddc433
Robbie Wagner <robbie.wagner@my.simpson.edu>
2022-10-25 19:20:42 -0500

    Updates to Doublets and Word :aerial_tramway:

--- src/DoubletsProblem.java
+++ src/DoubletsProblem.java
@@ -38,14 +38,24 @@ public class DoubletsProblem {
 
         for(String inputString : inputWords) {
             String[] wordToFrom = splitString(inputString);
-            //FindPath(wordToFrom[0], wordToFrom[1]);
+            //FindPath(wordToFrom[0], wordToFrom[1], dictionaryWords);
         }
 
     }
 
-    public static void FindPath(String firstWord, String finalWord) {
+    public static void FindPath(String firstWord, String finalWord, ArrayList<String> dictionaryWords) {
         if(firstWord.length() != finalWord.length()) System.out.println("no solution");
         else {
+            ArrayList<Word> wordTree = new ArrayList<Word>();
+
+            Word startWord = new Word(firstWord, null, 0, true, null);
+            boolean doneSearching = false;
+            Word currentWord = startWord;
+
+            while(!doneSearching)
+            {
+                addChildren(wordTree, currentWord, dictionaryWords, finalWord);
+            }
             //build a "tree" starting with the firstWord
             //add words from the dictionary with the same length and one letter difference
                 //(For optimization, consider creating a function that returns a list of strings with all words of same length)
@@ -62,17 +72,30 @@ public class DoubletsProblem {
         }
     }
 
-    public static void addChildren(){
-        // String[] wordsWithOneLetterDifference
-        // ArrayList<Word> addedWords = new ArrayList<Word>
-        // for string in wordsWithOneLetterDifference
-        //      if(!parentWord.unaddableWords.contains(string))
-        //          addedWords.add(new Word(String[string], new ArrayList<String>, findWeight(), true, parentWord)
-        //
-        // for addedWord in addedWords
-        //      unaddableWords.add(parentWord.word)
-        //      for word in addedWords
-        //          addedWords.unaddableWords.add(word.word)
+    public static void addChildren(ArrayList<Word> currentTree, Word parentWord, ArrayList<String> dictionaryWords, String finalWord){
+        ArrayList<String> wordsWithOneLetterDifference = findWordsWithOneLetterDifference(dictionaryWords, parentWord.word);
+        ArrayList<Word> addedWords = new ArrayList<Word>();
+
+        //Check to see if the parent word has unaddable words
+        if(parentWord.unaddableWords != null) {
+            for (String string : wordsWithOneLetterDifference) {
+                if(!parentWord.unaddableWords.contains(string)) {
+                addedWords.add(new Word(string, new ArrayList<String>(), findWeight(parentWord.word, string, finalWord), true, parentWord));
+                }
+            }
+        }
+        else {
+            for (String string : wordsWithOneLetterDifference) {
+                addedWords.add(new Word(string, new ArrayList<String>(), findWeight(parentWord.word, string, finalWord), true, parentWord));
+            }
+        }
+
+        for(Word addedWord : addedWords) {
+            addedWord.unaddableWords.add(parentWord.word);
+            for (Word word : addedWords) {
+                addedWord.unaddableWords.add(word.word);
+            }
+        }
     }
 
     public static ArrayList<String> findSameLengthWords(ArrayList<String> dictionary, int wordSize) {
@@ -87,8 +110,18 @@ public class DoubletsProblem {
         return sameLengthDictionaryWords;
     }
 
+    //UNIMPLEMENTED
+    public static ArrayList<String> findWordsWithOneLetterDifference(ArrayList<String> dictionary, String word) {
+        return null;
+    }
+
     public static String[] splitString(String string) {
         return string.split(" ");
+    }
+
+    //UNIMPLEMENTED
+    public static int findWeight(String parentString, String currentString, String goalString) {
+        return 0;
     }
 
     public static boolean isWord1LetterDifferent (String word1, String word2) {
--- src/Word.java
+++ src/Word.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Word {
 
     public final String word;
-    private ArrayList<String> unaddableWords;
+    public ArrayList<String> unaddableWords;
     public final int weight;
     public boolean isLeaf;
     public Word lastWord;
@@ -14,10 +14,6 @@ public class Word {
         this.weight = weight;
         this.isLeaf = isLeaf;
         this.lastWord = lastWord;
-    }
-
-    public void addUnaddableWord(Word unaddableWord) {
-        unaddableWords.add(unaddableWord.word);
     }
 
     public void isNoLongerLeaf() {
